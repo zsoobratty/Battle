41a3b07ef2e694187a82e2f0c5b90a512c033829
Ovy95 <jackoverton21@hotmail.com>
2020-02-04 16:27:57 +0000

    failed test

--- app.rb
+++ app.rb
@@ -2,9 +2,18 @@ require "sinatra/base"
 
 class Battle < Sinatra::Base
   get "/" do 
-    "hello battle"
+    "Testing infrastructure working!"
+  end
+  run! if app_file == $0
+
+  get "/" do 
+    erb :index
   end
 
-  run! if app_file == $0
+  post "/names" do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
   
 end
